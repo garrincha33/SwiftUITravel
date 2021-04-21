9708cb00ec665104fa2aa73286c63f0fbb7b3d67
Richard Price <mrspondooli@gmail.com>
2021-04-21 08:37:57 +0100

    added a scrollable discoverview

--- SwiftUITravel/ContentView.swift
+++ SwiftUITravel/ContentView.swift
@@ -9,17 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        //Step 1 create a NavView, scrollView, Hstack with frames and spacers
         NavigationView {
-            Text("Hello, world and test!")
-                .padding()
-                .navigationTitle("Loading")
+            // everything will be a scrollable view then a HStack Scroll view at the top
+            //use a foreach if creating more than one object
+            ScrollView {
+                DiscoverCategoriesView()
+            }.navigationTitle("Discover")
         }
-     
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+//extract to subview once code typed, or just create this View from the start
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        //step 2 put everything inside a Horizontal scroll view
+        //showsIndicators turns off the ugly scrollbar
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(0..<5, id: \.self) { num in
+                    //step to add text underneath your circles
+                    //use a Vstack
+                    VStack {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity) //infinity makes a full circle
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }//step 3 add some padding to HStack, use a background color for debugging
+            //.background(Color.red)
+            .padding(.horizontal)
+        }
     }
 }
